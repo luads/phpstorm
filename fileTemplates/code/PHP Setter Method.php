5f34f4d80d4551dcc1431a9de73f9366e4fc6622
Luã de Souza <lua.desouza@theiconic.com.au>
2018-11-19 17:30:54 +1100

    PS-2018.2.2 <lsouza@homac48.local Overwrite remote git@github.com:luads/phpstorm.git to local

--- fileTemplates/code/PHP Setter Method.php
+++
@@ -1,8 +0,0 @@
-public ${STATIC} function set${NAME}(#if (${SCALAR_TYPE_HINT})${SCALAR_TYPE_HINT} #else#end$${PARAM_NAME})
-{
-#if (${STATIC} == "static")
-    self::$${FIELD_NAME} = $${PARAM_NAME};
-#else
-    $this->${FIELD_NAME} = $${PARAM_NAME};
-#end
-}
